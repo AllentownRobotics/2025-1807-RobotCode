a6fbfd1b7779e4d4b8ac77ebfc26a49223d32e03
hueye <114522370+hueye@users.noreply.github.com>
2025-02-18 19:08:33 -0500

    updates!

--- src/main/java/frc/robot/subsystems/Blinkin.java
+++ src/main/java/frc/robot/subsystems/Blinkin.java
@@ -6,12 +6,116 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class blinkin extends SubsystemBase {
-  /** Creates a new blinkin. */
-  public blinkin() {}
+public class Blinkin extends SubsystemBase {
+  SparkMax blinkin;
+  /** Creates a new Blinkin. */
+  public Blinkin() {}
+  /*public void red(){
+    set(0.61);
+  }
+  public void darkRed(){
+    set(0.59);
+  }
+  public void hotPink(){
+    set(0.57);
+  
+  }
+  public void orange(){
+    set(0.65);
+  }
+  public void redOrange(){
+    set(0.63);
+  }
+  public void gold(){
+    set(0.67);
+  }
+  public void yellow(){
+    set(0.69);
+  }
+  public void lawnGreen(){
+    set(0.71);
+  }
+  public void lime(){
+    set(0.73);
+  }
+  public void darkGreen(){
+    set(0.75);
+  }
+  public void green(){
+    set(0.77);
+}
+  public void blueGreen(){
+    set(0.79);
+  }
+  public void aqua(){
+    set(0.81);
+  }
+  public void skyBlue(){
+    set(0.83);
+  }
+  public void darkBlue(){
+    set(0.85);
+  }
+  public void blue(){
+    set(0.87);
+  }
+  public void blueViolet(){
+    set(0.89);
+  }
+  public void violet(){
+    set(0.91);
+  }
+  public void white(){
+    set(0.93);
+  }
+  public void gray(){
+    set(0.95);
+  }
+  public void darkGray(){
+    set(0.97);
+  }
+  public void black(){
+    set(0.99);
+  }
 
+  //fixed palette patterns
+
+  public void rainbowRainbowPalette(){
+    set(-0.99);
+  }
+  public void rainbowPartyPalette(){
+    set(-0.97);
+  }
+  public void rainbowOceanPalette(){
+    set(-0.95);
+  }
+  public void rainbowLavePalette(){
+    set(-0.93);
+  }
+  public void rainbowForestPalette(){
+    set(-0.91);
+  }
+  public void rainbowWithGlitter(){
+    set(-0.89);
+  }
+  public void confetti(){
+    set(-0.87);
+  }
+  public void shotRed(){
+    set(-0.85);
+  }
+  public void shotBlue(){
+    set(-0.83);
+  }
+  public void shotWhite(){
+    set(-0.81);
+  }*/
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
+
+
+    //solid colors:
+    
+}
 }
