b745ce26360dc40f2d2d3dfd253cbf2cb373a101
diyap52 <parikh.d.2027@ufrsd.us>
2025-02-20 15:12:34 -0500

    general blinkin code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,8 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.util.Units;
-
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,73 +13,7 @@ import edu.wpi.first.math.util.Units;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-
-  // Controllers
-  public static class ControllerConstants {
-    public static final int driverControllerPort = 0;
-    public static final int operatorControllerPort = 1;
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
   }
-
-  public static class GlobalConstants {
-    // power distribution hub
-    public static final int pdhID = 13;
-    // Gyro ID
-    public static final int pigeonID = 14;
-  }
-
-  // Auto Constants
-  public static class AutoConstants { // ADJUST ALL
-    public static final double AUTO_MAX_SPEED_MPS = 5;
-
-    public static final double driveTrainRadius = Units.inchesToMeters(Math.sqrt(Math.pow(12, 2) + Math.pow(12, 2))); // in meters
-
-    public static final double PX_CONTROLLER = 5;
-    public static final double P_THETA_CONTROLLER = 1;
-  }
-
-  // Elevator Constants
-  public static class ElevatorConstants {
-    public static final int elevatorMotorID = 15;
-
-  // elevator motor PID
-    public static final double ELEVATOR_P = 0.01; // ADJUST ALL
-    public static final double ELEVATOR_I = 0.001;
-    public static final double ELEVATOR_D = 0;
-    public static final double ELEVATOR_SFF = 0; // static feedforward
-    public static final double ELEVATOR_VFF = 0; // velocity feedforward
-    public static final double ELEVATOR_AFF = 0; // acceleration feedforward
-    public static final double ELEVATOR_MIN_OUTPUT = -1;
-    public static final double ELEVATOR_MAX_OUTPUT = 1;
-  }
-
-  // Placer Constants
-  public static class PlacerConstants {
-    public static final int backMotorID = 16;
-    public static final int frontMotorID = 17;
-  }
-
-  // Climb Constants
-  public static class ClimbConstants {
-    public static final int climbMotorID = 18;
-
-  // climb motor PID
-    public static final double CLIMB_P = 0.01; // ADJUST ALL
-    public static final double CLIMB_I = 0.001;
-    public static final double CLIMB_D = 0;
-    public static final double CLIMB_SFF = 0; // static feedforward
-    public static final double CLIMB_VFF = 0; // velocity feedforward
-    public static final double CLIMB_AFF = 0; // acceleration feedforward
-    public static final double CLIMB_MIN_OUTPUT = -1;
-    public static final double CLIMB_MAX_OUTPUT = 1;
-  }
-
-  // Blinkin Constants
-  public static class BlinkinConstants {
-    public static final int blinkinID = 19;
-
-  // light codes
-    public static final double DARK_RED = 0.59; // temporary default
-    public static final double OCEAN_PALETTE_WAVES = -0.41; // temporary climbing
-  }
-
 }
--- src/main/java/frc/robot/subsystems/Blinkin.java
+++ src/main/java/frc/robot/subsystems/Blinkin.java
@@ -4,20 +4,11 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.BlinkinConstants;
 
-public class Blinkin extends SubsystemBase {
-  Spark blinkin;
-  /** Creates a new Blinkin. */
-  public Blinkin() {
-    blinkin = new Spark(BlinkinConstants.blinkinID);
-  }
-
-  public void setColor(double color) {
-    blinkin.set(color);
-  }
+public class blinkin extends SubsystemBase {
+  /** Creates a new blinkin. */
+  public blinkin() {}
 
   @Override
   public void periodic() {
