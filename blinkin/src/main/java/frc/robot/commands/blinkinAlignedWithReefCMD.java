13aebe7dd2df6936436d0eb9bc1872530d5a3cc1
diyap52 <parikh.d.2027@ufrsd.us>
2025-02-25 16:11:42 -0500

    updated blinkin code

---
+++ blinkin/src/main/java/frc/robot/commands/blinkinAlignedWithReefCMD.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.blinkin;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class blinkinAlignedWithReefCMD extends Command {
+  /** Creates a new blinkinCommands. */
+  public blinkinAlignedWithReefCMD() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+  private boolean flashing = false;
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    blinkin.color(OperatorConstants.alignedWithReef);
+
+    }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
